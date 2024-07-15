76f59dd2bfd56f91a717aa880356b2876b2fb333
laismilenam <140639353+laismilenam@users.noreply.github.com>
2024-07-15 17:04:28 -0300

    Update urls.py
    aumentando a segurança da api

--- urls.py
+++ urls.py
@@ -1,8 +1,20 @@
-from django.urls import path
-from .views import TopicListCreateView, TopicDetailView, CommentListCreateView
 
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 urlpatterns = [
     path('topics/', TopicListCreateView.as_view(), name='topic-list-create'),
     path('topics/<int:pk>/', TopicDetailView.as_view(), name='topic-detail'),
     path('comments/', CommentListCreateView.as_view(), name='comment-list-create'),
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('forum.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
